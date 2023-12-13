41ac6e5727d72ef65e08258b51abd403fc7c4379
JCZams1 <jessezamani456@gmail.com>
2023-12-13 14:47:51 -0800

    adding environ module

--- environ.c
+++ environ.c
@@ -5,7 +5,7 @@
  *
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- * 
+ *
  * Return: Always 0
  */
 int _myenv(info_t *info)
@@ -16,9 +16,9 @@ int _myenv(info_t *info)
 
 /**
  * _getenv - gets the value of an environ variable
- * 
+ *
  * @info: Structure containing potential arguments. Used to maintain
- * 
+ *
  * @name: env var name
  *
  * Return: the value
@@ -41,10 +41,10 @@ char *_getenv(info_t *info, const char *name)
 /**
  * _mysetenv - Initialize a new environment variable,
  *             or modify an existing one
- * 
+ *
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
- * 
+ *
  * Return: Always 0
  */
 int _mysetenv(info_t *info)
@@ -61,10 +61,10 @@ int _mysetenv(info_t *info)
 
 /**
  * _myunsetenv - Remove an environment variable
- * 
+ *
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
- * 
+ *
  * Return: Always 0
  */
 int _myunsetenv(info_t *info)
@@ -84,10 +84,10 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates env linked list
- * 
+ *
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- * 
+ *
  * Return: Always 0
  */
 int populate_env_list(info_t *info)
