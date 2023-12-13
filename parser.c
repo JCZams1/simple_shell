77f11d3bc17c6d53b70bc6fd9b05e41b3f414130
JCZams1 <jessezamani456@gmail.com>
2023-12-13 15:23:11 -0800

    add functionality to code

--- parser.c
+++ parser.c
@@ -2,15 +2,15 @@
 
 /**
  * Parsing, syntax analysis, or syntactic analysis is the process of analyzing a string of symbols,
- * 
+ *
  * either in natural language, computer languages or data structures
  */
 
 /**
  * is_cmd - determines if a file is an executable command
- * 
+ *
  * @info: the info struct
- * 
+ *
  * @path: path to the file
  *
  * Return: 1 if true, 0 otherwise
@@ -32,11 +32,11 @@ int is_cmd(info_t *info, char *path)
 
 /**
  * dup_chars - duplicates characters
- * 
+ *
  * @pathstr: the PATH string
- * 
+ *
  * @start: starting index
- * 
+ *
  * @stop: stopping index
  *
  * Return: pointer to new buffer
@@ -55,11 +55,11 @@ char *dup_chars(char *pathstr, int start, int stop)
 
 /**
  * find_path - finds this cmd in the PATH string
- * 
+ *
  * @info: the info struct
- * 
+ *
  * @pathstr: the PATH string
- * 
+ *
  * @cmd: the cmd to find
  *
  * Return: full path of cmd if found or NULL
